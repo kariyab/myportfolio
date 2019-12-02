158f0ece4fce838b37650ec04e45074a86fbc015
kariya <sgkuarut@yahoo.co.jp>
2019-12-02 23:00:32 +0900

    00.04

---
+++ resources/lang/ja/messages.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'Login' => 'ログイン',
+    'Logout' => 'ログアウト',
+    'E-Mail Address' => 'メールアドレス',
+    'Password' => 'パスワード',
+    'Forgot Your Password?' => 'パスワードを忘れましたか？',
+
+    'Remember Me' => 'ログイン情報を記憶する',
+    
+    'Register' => '登録',
+    'Name' => '名前',
+    'Confirm Password' => 'パスワード確認',
+    
+];
